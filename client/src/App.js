60fe51157f754e1f6153218cec882c77e62859da
Teekam Singh <teekams10999@gmail.com>
2022-05-04 17:51:42 +0530

    first-commit-4-may-22

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
+  return (
+    <h3> You will find my articles here soon...</h3>
+  );
+}
+
+export default App;
